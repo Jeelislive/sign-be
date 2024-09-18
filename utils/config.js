7767e9e9afcff656d73ebacc7c58ab8f741b6d5c
JEEL RUPARELIYA <108856045+Jeelislive@users.noreply.github.com>
2024-09-18 10:42:22 +0530

    config changes

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ const corsOptions = {
         "http://localhost:5173",
         "http://localhost:4173",
         "http://localhost:3000",
-        // process.env.CLIENT_URL,
+         process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
